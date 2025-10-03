4b94de573a999d3143cb71734f8a08795b2d4506
FireantT <97504862+FireantT@users.noreply.github.com>
2025-10-03 09:20:43 +0200

    Initialize models module with engine imports

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+from .api_worker import APIWorker
+from .face_detection_engine import FaceDetectionEngine
+from .authentication_engine import AuthenticationEngine
+from .communication_engine import CommunicationEngine
+
+__all__ = [
+    'APIWorker',
+    'FaceDetectionEngine', 
+    'AuthenticationEngine',
+    'CommunicationEngine'
+]
